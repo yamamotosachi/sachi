e54bff3dd425e0d9de208ce78444afd03d74fa00
yamamotosachi <a24.brst@chuo-u.ac.jp>
2024-11-19 17:36:33 +0900

    Initial Commit

---
+++ sachi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sachi
+//
+//  Created by 山本　倖  on 2024/11/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sachi/sachiApp.swift
@@ -0,0 +1,17 @@
+//
+//  sachiApp.swift
+//  sachi
+//
+//  Created by 山本　倖  on 2024/11/19.
+//
+
+import SwiftUI
+
+@main
+struct sachiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
